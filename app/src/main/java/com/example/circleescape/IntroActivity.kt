ced38f88e030c6f43f6445b747b1c9c051c60d1b
KyungChan <philip9504@naver.com>
2024-05-31 10:26:25 +0900

    ㆍ전체 API 버전 변경 : compileSdk ,targetSdk 33 → 34 상단바 제거 : theme > windowNoTitle 폴더 추가 : res > raw, res > anim 샘플 파일 추가 : sampleimg, samplemusic
    ㆍIntro
    화면 터치시 메인으로 이동 : onTouchEvent()
    인트로 경고창, 타이틀 이미지 임시 적용 : sampleimg
    인트로 경고창, 타이틀 이미지 차례대로 나타나도록 구현 : postDelayed
    인트로 페이드인,아웃 애니메이션 적용 : fadeinanimation.xml, fadeoutanimation.xml
    > 해당 모든 기능을 fragment로 변경 예정
    
    ㆍMain
    타이틀 텍스트, 게임시작 버튼 추가
    버튼 클릭시 플레이로 이동
    
    ㆍPlay
    fragment 개념 추가 예정

--- app/src/main/java/com/example/circleescape/IntroActivity.kt
+++ app/src/main/java/com/example/circleescape/IntroActivity.kt
@@ -1,12 +1,23 @@
 package com.example.circleescape
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.circleescape.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var introBinding: ActivityIntroBinding
+
+    // 애니메이션을 지연 초기화하기 위해 lateinit 사용
+    private lateinit var fadeinAnim: Animation
+    private lateinit var fadeoutAnim: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +26,42 @@ class IntroActivity : AppCompatActivity() {
         introBinding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(introBinding.root)
 
-        // 화면 터치시 메인으로 이동
+        val warntext1 = introBinding.textView1
+        val warntext2 = introBinding.textView2
+        val introtitleimg = introBinding.imageView
 
+        // 애니메이션 객체
+        val fadeinAnim = AnimationUtils.loadAnimation(this, R.anim.fadeinanimation)
+        val fadeoutAnim = AnimationUtils.loadAnimation(this, R.anim.fadeoutanimation)
 
+        introtitleimg.visibility = View.INVISIBLE
 
+        // 3초간 경고 텍스트를 보여줌
+        Handler(Looper.getMainLooper()).postDelayed({
+            warntext1.startAnimation(fadeinAnim)
+            warntext2.startAnimation(fadeinAnim)
+
+            warntext1.visibility = View.INVISIBLE
+            warntext2.visibility = View.INVISIBLE
+
+            introtitleimg.startAnimation(fadeoutAnim)
+
+            introtitleimg.visibility = View.VISIBLE
+        }, 5000)
+
+    }
+
+    // 액티비티 클래스에 터치이벤트 콜백 함수 onTouchEvent() 선언
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            // 화면에 손을 눌렀다 땠을 때(올렸을 때)
+            MotionEvent.ACTION_UP-> {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+
+                // overrideActivityTransition(Activity.OR.anim.fadeinanimation,R.anim.fadeoutanimation)
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
--- app/src/main/java/com/example/circleescape/MainActivity.kt
+++ app/src/main/java/com/example/circleescape/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.circleescape
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.circleescape.databinding.ActivityIntroBinding
+import com.example.circleescape.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // 뷰 바인딩 완료
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        val StartButton = mainBinding.startbutton
+        StartButton.setOnClickListener {
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
